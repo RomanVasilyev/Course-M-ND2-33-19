52f1dc375be1f7cc6795e4f871aad1086de18fb4
Vasilyev Roman <ammerhain@gmail.com>
2019-03-18 20:07:30 +0300

    Add Vasilyev dir

---
+++ Vasilyev/HometaskN1/BookLibrary/BookLibrary/IBookService.cs
@@ -0,0 +1,10 @@
+﻿namespace BookLibrary
+{
+    public interface IBookService
+    {
+        Book Get(int id);
+        void Add(Book book);
+        void Change(int id, string title);
+        void Delete(int id);
+    }
+}
---
+++ Vasilyev/HometaskN1/HometaskN1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using BookLibrary;
+using System.Threading;
+
+namespace HometaskN1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Select what do you want to do with a Book in Repository...");
+            Console.WriteLine("0. Get");
+            Console.WriteLine("1. Add");
+            Console.WriteLine("2. Change");
+            Console.WriteLine("3. Delete");
+            var BookService = new BookService(new BookRepository());
+            ConsoleKeyInfo key;
+            do
+            {
+                key = Console.ReadKey();
+                switch (key.Key)
+                {
+                    case ConsoleKey.D0:
+                        Console.WriteLine("\nEnter Id...");
+                        break;
+                    case ConsoleKey.D1:
+                        Console.WriteLine("\nEnter Title...");
+                        break;
+                    case ConsoleKey.D2:
+                        Console.WriteLine("\nEnter Id & new Title...");
+                        break;
+                    case ConsoleKey.D3:
+                        Console.WriteLine("\nEnter Id...");
+                        break;
+                    default:
+                        break;
+                }
+            } while (key.Key != ConsoleKey.Escape);
+            Console.ReadLine();
+        }
+    }
+}
