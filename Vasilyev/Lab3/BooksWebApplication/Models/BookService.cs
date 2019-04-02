b66c8f7005c8f8828f4dffa2cbf883c618f79589
Vasilyev Roman <ammerhain@gmail.com>
2019-04-02 16:01:59 +0300

    Lab 2 added

---
+++ Vasilyev/Lab3/BooksWebApplication/Models/BookService.cs
@@ -0,0 +1,18 @@
+﻿namespace Models
+{
+    public class BookService : IBookService
+    {
+        private readonly IRepository<Book> repository;
+
+        public BookService(IRepository<Book> repository)
+        {
+            this.repository = repository;
+        }
+
+        public Book Get(int id)
+        {
+            return repository.Get(id);
+        }
+
+    }
+}
